a9f2fc3b016d539f00064eac83ae26626be3e8d0
Michel Kekerovic <michelkekerovic@MichelmatoMacBook-Pro.local>
2020-04-29 14:57:18 -0700

    normal mode

---
+++ common-words.js
@@ -0,0 +1,66 @@
+// Here are the 100 most popular words in English, as totally
+// stolen from here: https://gist.github.com/gravitymonkey/2406023
+const commonWords = [
+    "the","of","and","a","to","in","is","you","that","it","he",
+    "was","for","on","are","as","with","his","they","I","at","be",
+    "this","have","from","or","one","had","by","word","but","not",
+    "what","all","were","we","when","your","can","said","there",
+    "use","an","each","which","she","do","how","their","if","will",
+    "up","other","about","out","many","then","them","these","so",
+    "some","her","would","make","like","him","into","time","has",
+    "look","two","more","write","go","see","number","no","way",
+    "could","people","my","than","first","water","been","call",
+    "who","oil","its","now","find","long","down","day","did","get",
+    "come","made","may","part"
+  ]
+
+  const words = commonWords.filter(word => word.lenght >= 3)
+
+  const randIndex = Math.floor(Math.random() * words.length)
+  var turns = 5
+  $("#turns").html(turns + " guesses left")
+
+  const answer = words[randIndex]
+  console.log(answer)
+  const underscores = answer.split("").map(l => "_")
+
+  $("#underscores").html(underscores.join(""))
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  var buttonshtml = ""
+  alphabet.split("").forEach(l => {
+      buttonshtml += `<button>${l}</button>`
+  })
+  $("#buttons").html(buttonshtml)
+
+  $("#buttons").on("click","button", function(e) {
+      e.preventDefault()
+
+      $(this).attr('disabled', true)
+
+      var val = $(this).html()
+
+      if (answer.indexOf(val) === -1) {
+        turns -= 1
+        if (turns === 0) {
+            $("#game").html("<h1>You Lose:(</h1>")
+
+        } else {
+          $("#turns").html(turns + " guesses left")
+        }
+      } else { 
+        answer.split("").forEach((l, i) => {
+            if (val === l) {
+                underscores[i] = l
+  
+                if (underscores.indexOf("_") === -1) {
+                    //game win
+                    $("#game").html("<h1>You Win!</h1>")
+                }
+            }
+        })
+
+        $("#underscores").html(underscores.join(""))
+    }
+})
