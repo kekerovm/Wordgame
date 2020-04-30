e01bdf6ac756212b5d738008383fa6a1945e024a
Michel Kekerovic <michelkekerovic@MichelmatoMacBook-Pro.local>
2020-04-29 19:01:14 -0700

    hard mode two

--- main.js
+++ main.js
@@ -1,155 +1,73 @@
-$(document).ready(function() {
-var commonWords = [
-  "the",
-  "of",
-  "and",
-  "a",
-  "to",
-  "in",
-  "is",
-  "you",
-  "that",
-  "it",
-  "he",
-  "was",
-  "for",
-  "on",
-  "are",
-  "as",
-  "with",
-  "his",
-  "they",
-  "I",
-  "at",
-  "be",
-  "this",
-  "have",
-  "from",
-  "or",
-  "one",
-  "had",
-  "by",
-  "word",
-  "but",
-  "not",
-  "what",
-  "all",
-  "were",
-  "we",
-  "when",
-  "your",
-  "can",
-  "said",
-  "there",
-  "use",
-  "an",
-  "each",
-  "which",
-  "she",
-  "do",
-  "how",
-  "their",
-  "if",
-  "will",
-  "up",
-  "other",
-  "about",
-  "out",
-  "many",
-  "then",
-  "them",
-  "these",
-  "so",
-  "some",
-  "her",
-  "would",
-  "make",
-  "like",
-  "him",
-  "into",
-  "time",
-  "has",
-  "look",
-  "two",
-  "more",
-  "write",
-  "go",
-  "see",
-  "number",
-  "no",
-  "way",
-  "could",
-  "people",
-  "my",
-  "than",
-  "first",
-  "water",
-  "been",
-  "call",
-  "who",
-  "oil",
-  "its",
-  "now",
-  "find",
-  "long",
-  "down",
-  "day",
-  "did",
-  "get",
-  "come",
-  "made",
-  "may",
-  "part"
-];
+// Here are the 100 most popular words in English, as totally
+// stolen from here: https://gist.github.com/gravitymonkey/2406023
+const commonWords = [
+  "the","of","and","a","to","in","is","you","that","it","he",
+  "was","for","on","are","as","with","his","they","I","at","be",
+  "this","have","from","or","one","had","by","word","but","not",
+  "what","all","were","we","when","your","can","said","there",
+  "use","an","each","which","she","do","how","their","if","will",
+  "up","other","about","out","many","then","them","these","so",
+  "some","her","would","make","like","him","into","time","has",
+  "look","two","more","write","go","see","number","no","way",
+  "could","people","my","than","first","water","been","call",
+  "who","oil","its","now","find","long","down","day","did","get",
+  "come","made","may","part"
+]
 
-//choosing a random word
-var word = words[Math.floor(Math.random() * words.length)];
+const words = commonWords.filter(word => word.length >= 3)
 
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
-} 
+const randIndex = Math.floor(Math.random() * words.length)
+var turns = 5
+var guesses = 0
 
-var remainingletters = word.length;
+const answer = words[randIndex]
+console.log(answer)
+const underscores = answer.split("").map(l => "_")
 
-//Coding the game loop
+$("#underscores").html(underscores.join(""))
 
-while (remainingletters > 0) {
-  alert(answerArray.join(" "));
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-///Handling the player's input
+var buttonshtml = ""
+alphabet.split("").forEach(l => {
+    buttonshtml += `<button>${l}</button>`
+})
+$("#buttons").html(buttonshtml)
 
-  var guess = prompt("Guess a letter, or Click cancel to stop playing.");
+$("#buttons").on("click","button", function(e) {
+    e.preventDefault()
 
-  if (guess === null) {
+    $(this).attr("disabled", true)
 
-///Exit the game loop
-   break;
- } else if (guess.length !== 1) {
-   alert("Please enter a letter");
- } else {
+    var val = $(this).html()
 
-///Updating the game state
-  for (var j = 0; j < word.length; j++) {
-    if (word[j] === guess) {
-      answerArray[j] = guess;
-      remainingletters--;
-    }
+    if (answer.indexOf(val) === -1) {
+      guesses += 1
+      if (guesses === turns) {
+        var perc = (100 / turns) * (turns - guesses)
+        console.log(perc)
+        $("meter").css('width', perc + "%")
+        setTimeout(() => {
+          $("#game").html("<h1>Try Again!</h1>")
+        }, 1000)
+      } else {
+        var perc = (100 / turns) * (turns - guesses)
+        console.log(perc)
+        $("#meter").css('width', perc + "%")
+      //   $("#turns").html(turns + " guesses left")
+      }
+    } else { 
+      answer.split("").forEach((l, i) => {
+          if (val === l) {
+              underscores[i] = l
+
+              if (underscores.indexOf("_") === -1) {
+                  //game win
+                  $("#game").html("<h1>You Win!</h1>")
+              }
+          }
+      })
+
+      $("#underscores").html(underscores.join(""))
   }
-}
-///The end of the game loop
-}
-// ///New array containing 3 letters or more
-// var words = commonWords.filter(function(item) {
-//     return item.length >= 3
-// })
-
-///Ending the game
-
-alert(answerArray.join(" "));
-alert("Great job! The answer was " + word);
-
-// document.getElementById("result").innerHTML = answerArray.join("")
-// document.getElementById("maxWrong").innerHTML = maxwrong
-
-// document.getElementById("letters").onClick = function guessedLetter())
+})
